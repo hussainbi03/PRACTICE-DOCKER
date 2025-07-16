fe7f493a657424a38e51950991769e4cc2fe31ed
hussainbi03 <hussainbis03@gmail.com>
2025-07-16 19:14:24 +0530

    docker commands

--- DOCKER/docker.tf
+++ DOCKER/docker.tf
@@ -2,12 +2,15 @@ resource "aws_instance" "this" {
     ami = data.aws_ami.joindevops.id
     vpc_security_group_ids = [aws_security_group.allow_tls.id]
     instance_type = "t3.micro"
+    # 20GB is not enough
+    root_block_device {
+    volume_size = 50  # Set root volume size to 50GB
+    volume_type = "gp3"  # Use gp3 for better performance (optional)
+  }
     user_data = file("docker.sh")
     tags = {
         Name = "docker-demo"
-    }
-    
-    
+    } 
 }
 
  resource "aws_security_group" "allow_tls"{
